ecd32bea8fa29247d5b40387b43e01478174dfd9
Alejandro <alejandro.deleon@utp.ac.pa>
2021-12-26 08:17:14 -0500

    [WEB] tailwind init to Web

---
+++ apps/clients/web/tailwind.config.js
@@ -0,0 +1,53 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  presets: [require('../../../tailwind-workspace-preset.js')],
+  purge: [
+    join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(
+      __dirname,
+      '../../../tailwind.workspace-preset.js'
+    ),
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: () => ({
+        login: "url('/images/login_bg.png')",
+        hero: "url('/images/main_header.png')",
+        pattern1: 'church',
+        pattern2: 'endless',
+        pattern3: 'parkay',
+        pattern4: 'melt',
+        pattern5: 'formal',
+        pattern6: "url('/images/pattern1.png')",
+        pattern7: "url('/images/pattern2.png')",
+        pattern8: "url('/images/pattern3.png')",
+        pattern9: "url('/images/pattern4.png')",
+        pattern10: "url('/images/pattern5.png')",
+        footer: "url('/images/footer.svg')",
+        surface: "url('/images/surface.svg')",
+        teresa: "url('/images/teresaprofile.png')",
+        alejandro: "url('/images/alejandro.jpg')",
+        alek: "url('/images/alek.jpg')",
+        michael: "url('/images/michael.jpg')",
+        box: "url('/images/box.jpg')",
+        jose: "url('/images/jose.jpg')",
+        aristides: "url('/images/aristides.jpg')",
+      }),
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        xl: '1px 5px 6px -4px rgba(0,0,0,0.1)',
+      },
+      backdropContrast: {
+        25: '.25',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
